d310bc64c025e058bd7b52eabd296fca4528333c
bisolaaa <abisola.bayo-adisa@pau.edu.ng>
2025-10-10 17:52:45 +0100

    Delete week-2 directory

--- week-2/practice_2.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-	println!(); // prints just a
-	println!("hello ");//prints
-	println!("format {} arguements", "some"); //prints format some arguements
-	println!("My name is {}. I am the class rep of stream 1", "Dagogo William-Jumbo");
-}
