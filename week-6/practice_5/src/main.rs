e20b221f769c0f483d618df542bdc75cf0847096
bisolaaa <abisola.bayo-adisa@pau.edu.ng>
2025-11-05 16:59:06 +0000

    (5th practice)

---
+++ week-6/practice_5/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let fullname = " Pan-Atlantic University ";
+    println!();
+    println!("Name: {}", fullname);
+    println!();
+    println!("Length is {}",fullname.len());
+    println!();
+    println!("After trim ");
+    println!("Length is {}",fullname.trim().len());
+}
