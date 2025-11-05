7cb79bc2c9d4676df69e09ea1a9460ad228eb7e8
bisolaaa <abisola.bayo-adisa@pau.edu.ng>
2025-11-05 17:36:18 +0000

    (8th practice)

---
+++ week-6/practice_8/target/debug/deps/practice_8.d
@@ -0,0 +1,5 @@
+C:\Users\hp\Documents\abayo-adisaCOS101\week-6\practice_8\target\debug\deps\practice_8.d: src\main.rs
+
+C:\Users\hp\Documents\abayo-adisaCOS101\week-6\practice_8\target\debug\deps\practice_8.exe: src\main.rs
+
+src\main.rs:
---
+++ week-6/practice_8/target/debug/practice_8.d
@@ -0,0 +1,1 @@
+C:\Users\hp\Documents\abayo-adisaCOS101\week-6\practice_8\target\debug\practice_8.exe: C:\Users\hp\Documents\abayo-adisaCOS101\week-6\practice_8\src\main.rs
