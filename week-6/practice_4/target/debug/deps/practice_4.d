735072ef5bbbb915399c079e623613a59cbe40ca
bisolaaa <abisola.bayo-adisa@pau.edu.ng>
2025-11-05 15:50:59 +0000

    (4th practice)

---
+++ week-6/practice_4/target/debug/deps/practice_4.d
@@ -0,0 +1,5 @@
+C:\Users\hp\Documents\abayo-adisaCOS101\week-6\practice_4\target\debug\deps\practice_4.d: src\main.rs
+
+C:\Users\hp\Documents\abayo-adisaCOS101\week-6\practice_4\target\debug\deps\practice_4.exe: src\main.rs
+
+src\main.rs:
