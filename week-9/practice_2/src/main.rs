ebcdfd0dbb1abf7d30ee7033b614957aecf8fa08
bisolaaa <abisola.bayo-adisa@pau.edu.ng>
2025-11-26 13:25:45 +0100

    Reading from a file

---
+++ week-9/practice_2/src/main.rs
@@ -0,0 +1,11 @@
+use std::io::Read;
+use std::io::Write;
+
+fn main() {
+    let mut file = std::fs::File::create("welcome_message.txt").expect("create failed");
+    file.write_all("on this segment".as_bytes());
+   let mut file = std::fs::File::open("welcome_message.txt").unwrap();
+   let mut contents = String::new();
+   file.read_to_string(&mut contents).unwrap();
+   print!("{}", contents); 
+}
---
+++ week-9/practice_2/target/debug/deps/practice_2-26efaa8adbd2b5ca.d
@@ -0,0 +1,5 @@
+C:\Users\USER\Documents\abayo-adisaCOS101\week-9\practice_2\target\debug\deps\practice_2-26efaa8adbd2b5ca.d: src\main.rs
+
+C:\Users\USER\Documents\abayo-adisaCOS101\week-9\practice_2\target\debug\deps\libpractice_2-26efaa8adbd2b5ca.rmeta: src\main.rs
+
+src\main.rs:
